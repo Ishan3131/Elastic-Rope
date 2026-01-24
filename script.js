00b6a996e8f97808b407ca426b5b701931e807d1
Ishan3131 <chetwanikrish@gmail.com>
2026-01-24 19:46:22 +0530

    user can change constants through console

--- script.js
+++ script.js
@@ -6,30 +6,42 @@ let a = [0,0]
 let damping = .07;
 let walls = true;
 let magnitude = .5;
-let e = .9; 
+let e = .9;
+let state = false;
 
 document.addEventListener('mousemove', (e) => {
     x = e.clientX - 18;
     y = e.clientY - 18;
 })
 
-setInterval(() => {
-    // console.log('x',x,'y',y)
-    // console.log('p',p,'q',q)
-    let h = x-p;
-    let v = y-q;
+function start(frameGap){
+    if(!state){
+        interval = setInterval(() => {
+            state = true;
+            // console.log('x',x,'y',y)
+            // console.log('p',p,'q',q)
+            let h = x-p;
+            let v = y-q;
 
-    let vectorLengthReci = 1/Math.hypot(h,v);
-    a[0] =  h*vectorLengthReci;
-    a[1] = v*vectorLengthReci;
-    vel[0] += magnitude*a[0];
-    vel[1] += magnitude*a[1];
-    p += vel[0];
-    q += vel[1];
-    box.style.left = p + 'px';
-    box.style.top = q + 'px';
-    vel[0] -= damping*Math.sign(vel[0]);
-    vel[1] -= damping*Math.sign(vel[1]);
-    if(walls && (p >= window.innerWidth-50 || p <= 0)) vel[0] *= -e;
-    if(walls && (q >= window.innerHeight-50 || q <= 0)) vel[1] *= -e;
-},10)
+            let vectorLengthReci = 1/Math.hypot(h,v);
+            a[0] =  h*vectorLengthReci;
+            a[1] = v*vectorLengthReci;
+            vel[0] += magnitude*a[0];
+            vel[1] += magnitude*a[1];
+            p += vel[0];
+            q += vel[1];
+            box.style.left = p + 'px';
+            box.style.top = q + 'px';
+            vel[0] -= damping*Math.sign(vel[0]);
+            vel[1] -= damping*Math.sign(vel[1]);
+            if(walls && (p >= window.innerWidth-50 || p <= 0)) vel[0] *= -e;
+            if(walls && (q >= window.innerHeight-50 || q <= 0)) vel[1] *= -e;
+        },frameGap)
+    }
+}
+
+function stop(){
+    clearInterval(interval);
+    state = false;
+}
+start(10);
